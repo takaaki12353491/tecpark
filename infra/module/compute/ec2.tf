5d6ef4e7d66a9772ab4b8f830e57ee0c06b929df
takaaki12353491 <takaaki12353491@gmail.com>
2024-08-01 01:55:04 +0900

    ref: ec2の名前を整理

--- infra/module/compute/ec2.tf
+++ infra/module/compute/ec2.tf
@@ -1,4 +1,4 @@
-data "aws_iam_policy_document" "ec2_assume_role_policy_document" {
+data "aws_iam_policy_document" "ec2_assume_iam" {
   statement {
     effect = "Allow"
 
@@ -11,31 +11,31 @@ data "aws_iam_policy_document" "ec2_assume_role_policy_document" {
   }
 }
 
-resource "aws_iam_role" "ec2_assume_role" {
-  name               = "${var.project}-${var.env}-ec2-assume-role"
-  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role_policy_document.json
+resource "aws_iam_role" "ec2_service_access" {
+  name               = "ec2-service-access"
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_iam.json
 
   tags = merge(
     local.common_tags,
     {
-      Name = "${var.project}-${var.env}-ec2-assume-role"
+      Name = "ec2-service-access"
     }
   )
 }
 
-resource "aws_iam_role_policy_attachment" "ec2_ssm_policy_attachment" {
-  role       = aws_iam_role.ec2_assume_role.name
+resource "aws_iam_role_policy_attachment" "ec2_ssm_access" {
+  role       = aws_iam_role.ec2_service_access.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
-resource "aws_iam_instance_profile" "ec2_assume_role_profile" {
-  name = "${var.project}-${var.env}-ec2-assume-role-profile"
-  role = aws_iam_role.ec2_assume_role.name
+resource "aws_iam_instance_profile" "ec2_grant_service_access" {
+  name = "ec2-assume-role-profile"
+  role = aws_iam_role.ec2_service_access.name
 
   tags = merge(
     local.common_tags,
     {
-      Name = "${var.project}-${var.env}-ec2-assume-role-profile"
+      Name = "ec2-assume-role-profile"
     }
   )
 }
@@ -44,31 +44,31 @@ resource "aws_instance" "bastion_server" {
   ami           = "ami-0d03c6e00d5732e28" # Amazon Linux 2023
   instance_type = "t2.micro"
 
-  iam_instance_profile   = aws_iam_instance_profile.ec2_assume_role_profile.name
+  iam_instance_profile   = aws_iam_instance_profile.ec2_grant_service_access.name
   subnet_id              = var.public_subnet_1a_id
   vpc_security_group_ids = [var.security_group_bastion_id]
 
   tags = merge(
     local.common_tags,
     {
-      Name = "${var.project}-${var.env}-bastion-server"
+      Name = "bastion-server"
     }
   )
 }
 resource "aws_cloudwatch_log_group" "ssm_log_group" {
-  name              = "${var.project}-${var.env}-ssm-log-group"
+  name              = "ssm-log-group"
   retention_in_days = 7
 
   tags = merge(
     local.common_tags,
     {
-      Name = "${var.project}-${var.env}-ssm-log-group"
+      Name = "ssm-log-group"
     }
   )
 }
 
 resource "aws_ssm_document" "ssm_logging" {
-  name          = "${var.project}-${var.env}-ssm-logging"
+  name          = "ssm-logging"
   document_type = "Session"
 
   content = jsonencode({
@@ -84,7 +84,7 @@ resource "aws_ssm_document" "ssm_logging" {
   tags = merge(
     local.common_tags,
     {
-      Name = "${var.project}-${var.env}-ssm-logging"
+      Name = "ssm-logging"
     }
   )
 }
