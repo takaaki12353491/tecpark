2fc1868898a15a2677e28fe47db017f4d5e8f173
takaaki12353491 <takaaki12353491@gmail.com>
2024-07-31 21:18:03 +0900

    ref: routeのリソース名を修正

--- infra/module/network/route.tf
+++ infra/module/network/route.tf
@@ -1,49 +1,49 @@
-resource "aws_route_table" "public_rt" {
+resource "aws_route_table" "public" {
   vpc_id = aws_vpc.vpc.id
 
   tags = merge(
     local.common_tags,
     {
-      Name = "${var.project}-${var.env}-public-rt"
+      Name = "public"
       Type = "public"
     }
   )
 }
 
-resource "aws_route_table_association" "public_rt_1a" {
-  route_table_id = aws_route_table.public_rt.id
+resource "aws_route_table_association" "public_link_1a" {
+  route_table_id = aws_route_table.public.id
   subnet_id      = aws_subnet.public_subnet_1a.id
 }
 
-resource "aws_route_table_association" "public_rt_1c" {
-  route_table_id = aws_route_table.public_rt.id
+resource "aws_route_table_association" "public_link_1c" {
+  route_table_id = aws_route_table.public.id
   subnet_id      = aws_subnet.public_subnet_1c.id
 }
 
-resource "aws_route_table" "private_rt" {
+resource "aws_route" "public_internet_access" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+resource "aws_route_table" "private" {
   vpc_id = aws_vpc.vpc.id
 
   tags = merge(
     local.common_tags,
     {
-      Name = "${var.project}-${var.env}-private-rt"
+      Name = "private"
       Type = "private"
     }
   )
 }
 
-resource "aws_route_table_association" "private_rt_1a" {
-  route_table_id = aws_route_table.private_rt.id
+resource "aws_route_table_association" "private_link_1a" {
+  route_table_id = aws_route_table.private.id
   subnet_id      = aws_subnet.private_subnet_1a.id
 }
 
-resource "aws_route_table_association" "private_rt_1c" {
-  route_table_id = aws_route_table.private_rt.id
+resource "aws_route_table_association" "private_link_1c" {
+  route_table_id = aws_route_table.private.id
   subnet_id      = aws_subnet.private_subnet_1c.id
-}
-
-resource "aws_route" "public_route" {
-  route_table_id         = aws_route_table.public_rt.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.main.id
 }
