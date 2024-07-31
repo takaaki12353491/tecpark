9c299dbee02f3011f97892922164c454cba43bb4
takaaki12353491 <takaaki12353491@gmail.com>
2024-07-31 17:53:39 +0900

    feat: 踏み台サーバー用のsecurity groupを作成

--- infra/module/network/security_group.tf
+++ infra/module/network/security_group.tf
@@ -99,3 +99,34 @@ resource "aws_security_group_rule" "db_in_tcp3306" {
   to_port                  = 3306
   source_security_group_id = aws_security_group.app_sg.id
 }
+
+resource "aws_security_group" "bastion" {
+  name        = "${var.project}-${var.env}-bastion"
+  description = "bastion server role security group"
+  vpc_id      = aws_vpc.vpc.id
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.env}-bastion"
+    }
+  )
+}
+
+resource "aws_security_group_rule" "bastion_in_https" {
+  security_group_id = aws_security_group.bastion_sg.id
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 443
+  to_port           = 443
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "bastion_out_https" {
+  security_group_id = aws_security_group.bastion_sg.id
+  type              = "egress"
+  protocol          = "tcp"
+  from_port         = 443
+  to_port           = 443
+  cidr_blocks       = ["0.0.0.0/0"]
+}
