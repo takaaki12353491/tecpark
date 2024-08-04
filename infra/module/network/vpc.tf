e0199d50ad3481991d3752dfa7e1e9612af664e1
takaaki12353491 <takaaki12353491@gmail.com>
2024-08-05 02:13:41 +0900

    fix: 不要なvpc endpointを削除

--- infra/module/network/vpc.tf
+++ infra/module/network/vpc.tf
@@ -27,6 +27,7 @@ resource "aws_vpc_endpoint" "s3" {
   )
 }
 
+# トラフィックが多いのでendpointで料金を抑える
 resource "aws_vpc_endpoint" "logs" {
   vpc_id              = aws_vpc.main.id
   service_name        = data.aws_vpc_endpoint_service.logs.service_name
@@ -39,86 +40,6 @@ resource "aws_vpc_endpoint" "logs" {
     local.common_tags,
     {
       Name = "logs"
-    }
-  )
-}
-
-resource "aws_vpc_endpoint" "ecr_dkr" {
-  vpc_id              = aws_vpc.main.id
-  service_name        = data.aws_vpc_endpoint_service.ecr_dkr.service_name
-  vpc_endpoint_type   = "Interface"
-  subnet_ids          = aws_subnet.private[*].id
-  security_group_ids  = [aws_security_group.vpc_endpoint.id]
-  private_dns_enabled = true
-
-  tags = merge(
-    local.common_tags,
-    {
-      Name = "ecr-dkr"
-    }
-  )
-}
-
-resource "aws_vpc_endpoint" "ecr_api" {
-  vpc_id              = aws_vpc.main.id
-  service_name        = data.aws_vpc_endpoint_service.ecr_api.service_name
-  vpc_endpoint_type   = "Interface"
-  subnet_ids          = aws_subnet.private[*].id
-  security_group_ids  = [aws_security_group.vpc_endpoint.id]
-  private_dns_enabled = true
-
-  tags = merge(
-    local.common_tags,
-    {
-      Name = "ecr-dkr"
-    }
-  )
-}
-
-resource "aws_vpc_endpoint" "ssm" {
-  vpc_id              = aws_vpc.main.id
-  service_name        = data.aws_vpc_endpoint_service.ssm.service_name
-  vpc_endpoint_type   = "Interface"
-  subnet_ids          = aws_subnet.private[*].id
-  security_group_ids  = [aws_security_group.vpc_endpoint.id]
-  private_dns_enabled = true
-
-  tags = merge(
-    local.common_tags,
-    {
-      Name = "ssm"
-    }
-  )
-}
-
-resource "aws_vpc_endpoint" "ssmmessages" {
-  vpc_id              = aws_vpc.main.id
-  service_name        = "com.amazonaws.${data.aws_region.current.name}.ssmmessages"
-  vpc_endpoint_type   = "Interface"
-  subnet_ids          = aws_subnet.private[*].id
-  security_group_ids  = [aws_security_group.vpc_endpoint.id]
-  private_dns_enabled = true
-
-  tags = merge(
-    local.common_tags,
-    {
-      Name = "ssmmessages"
-    }
-  )
-}
-
-resource "aws_vpc_endpoint" "ec2messages" {
-  vpc_id              = aws_vpc.main.id
-  service_name        = "com.amazonaws.${data.aws_region.current.name}.ec2messages"
-  vpc_endpoint_type   = "Interface"
-  subnet_ids          = aws_subnet.private[*].id
-  security_group_ids  = [aws_security_group.vpc_endpoint.id]
-  private_dns_enabled = true
-
-  tags = merge(
-    local.common_tags,
-    {
-      Name = "ec2messages"
     }
   )
 }
