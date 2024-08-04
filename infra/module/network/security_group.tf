1bd3ba3d61427795f1c0fe7643e2262995d7726c
takaaki12353491 <takaaki12353491@gmail.com>
2024-08-05 02:06:15 +0900

    fix: security groupでアウトバウンドを修正

--- infra/module/network/security_group.tf
+++ infra/module/network/security_group.tf
@@ -21,7 +21,7 @@ resource "aws_security_group" "web" {
     protocol    = "tcp"
     from_port   = 80
     to_port     = 80
-    cidr_blocks = local.private_cidrs
+    cidr_blocks = [local.all_cidr]
   }
 
   tags = merge(
@@ -116,14 +116,14 @@ resource "aws_security_group" "bastion" {
     protocol    = "tcp"
     from_port   = 80
     to_port     = 80
-    cidr_blocks = local.public_cidrs
+    cidr_blocks = [local.all_cidr]
   }
 
   egress {
     protocol    = "tcp"
     from_port   = 443
     to_port     = 443
-    cidr_blocks = local.public_cidrs
+    cidr_blocks = [local.all_cidr]
   }
 
   egress {
