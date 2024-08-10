0235427e7321c25d8aaf2a56d28b63b0298d2369
takaaki12353491 <takaaki12353491@gmail.com>
2024-08-11 06:32:28 +0900

    ref: ファイルを整理

--- infra/module/bastion/ec2.tf
+++ infra/module/bastion/ec2.tf
@@ -1,28 +1,3 @@
-resource "aws_iam_role" "bastion" {
-  name               = "bastion"
-  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
-
-  managed_policy_arns = [
-    "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore",
-    "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess",
-    "arn:aws:iam::aws:policy/SecretsManagerReadWrite",
-    "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy",
-  ]
-
-  tags = {
-    Name = "bastion"
-  }
-}
-
-resource "aws_iam_instance_profile" "bastion" {
-  name = "bastion"
-  role = aws_iam_role.bastion.name
-
-  tags = {
-    Name = "bastion"
-  }
-}
-
 resource "aws_instance" "bastion" {
   ami           = "ami-0d03c6e00d5732e28" # Amazon Linux 2023
   instance_type = "t2.micro"
