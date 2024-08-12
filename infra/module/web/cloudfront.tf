ca95b0523bba20463021b9395ec5edb60fdd6ca0
takaaki12353491 <takaaki12353491@gmail.com>
2024-08-13 03:00:56 +0900

    fix: cloudfrontとalbの通信をhttpsのみ許可する

--- infra/module/web/cloudfront.tf
+++ infra/module/web/cloudfront.tf
@@ -11,7 +11,7 @@ resource "aws_cloudfront_distribution" "user" {
     custom_origin_config {
       http_port              = 80
       https_port             = 443
-      origin_protocol_policy = "match-viewer"
+      origin_protocol_policy = "https-only"
       origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
     }
 
--- infra/module/web/elb.tf
+++ infra/module/web/elb.tf
@@ -22,26 +22,6 @@ resource "aws_lb_target_group" "api" {
   }
 }
 
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.alb.arn
-  protocol          = "HTTP"
-  port              = 80
-
-  default_action {
-    type = "redirect"
-
-    redirect {
-      protocol    = "HTTPS"
-      port        = "443"
-      status_code = "HTTP_301"
-    }
-  }
-
-  tags = {
-    Name = "http"
-  }
-}
-
 resource "aws_lb_listener" "https" {
   load_balancer_arn = aws_lb.alb.arn
   protocol          = "HTTPS"
