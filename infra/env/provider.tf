e022259fdf3f4d6d320a412bea3882c32e86cc28
takaaki12353491 <takaaki12353491@gmail.com>
2024-09-06 18:45:09 +0900

    chore: providerのversionの設定を修正

--- infra/env/provider.tf
+++ infra/env/provider.tf
@@ -1,15 +1,15 @@
 terraform {
-  required_version = "~> 1.9.3"
+  required_version = "~> 1.9"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 5.66"
     }
 
     github = {
       source  = "integrations/github"
-      version = "~> 6.0"
+      version = "~> 6.2"
     }
   }
 }
