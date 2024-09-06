be9d888a4db0e860c73b9e1288168a489dfd1b2a
takaaki12353491 <takaaki12353491@gmail.com>
2024-09-06 20:22:13 +0900

    feat: AWS_GITHUB_ACTIONS_IAM_ROLE_ARNをGitHubに作成

--- infra/module/operation/github/github.tf
+++ infra/module/operation/github/github.tf
@@ -9,3 +9,10 @@ resource "github_actions_environment_variable" "aws_region" {
   variable_name = "AWS_REGION"
   value         = data.aws_region.current.name
 }
+
+resource "github_actions_environment_secret" "aws_github_actions_iam_role_arn" {
+  repository      = github_repository_environment.main.repository
+  environment     = github_repository_environment.main.environment
+  secret_name     = "AWS_GITHUB_ACTIONS_IAM_ROLE_ARN"
+  plaintext_value = aws_iam_role.github_actions.arn
+}
