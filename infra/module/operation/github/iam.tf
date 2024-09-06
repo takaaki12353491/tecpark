99b72010cdfacd28f8193cf4bf63c8983a6c18ea
takaaki12353491 <takaaki12353491@gmail.com>
2024-09-07 03:06:05 +0900

    ref: 空行を削除

--- infra/module/operation/github/iam.tf
+++ infra/module/operation/github/iam.tf
@@ -1,10 +1,8 @@
 resource "aws_iam_openid_connect_provider" "github_actions" {
   url = "https://token.actions.githubusercontent.com"
-
   client_id_list = [
     "sts.amazonaws.com"
   ]
-
   thumbprint_list = [
     data.tls_certificate.github_actions.certificates[0].sha1_fingerprint
   ]
@@ -18,7 +16,6 @@ resource "aws_iam_role" "github_actions" {
   name               = "github-actions"
   assume_role_policy = data.aws_iam_policy_document.github_actions.json
   description        = "IAM Role for GitHub Actions OIDC"
-
   managed_policy_arns = [
     "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"
   ]
