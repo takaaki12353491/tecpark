785fefb55638999167b6b28e8e02a02150148f02
takaaki12353491 <takaaki12353491@gmail.com>
2024-09-07 03:20:03 +0900

    fix: aws_github_actions_iam_role_arnをsecretからvariableに修正

--- infra/module/operation/github/github.tf
+++ infra/module/operation/github/github.tf
@@ -10,9 +10,9 @@ resource "github_actions_environment_variable" "aws_region" {
   value         = data.aws_region.current.name
 }
 
-resource "github_actions_environment_secret" "aws_github_actions_iam_role_arn" {
-  repository      = github_repository_environment.main.repository
-  environment     = github_repository_environment.main.environment
-  secret_name     = "AWS_GITHUB_ACTIONS_IAM_ROLE_ARN"
-  plaintext_value = aws_iam_role.github_actions.arn
+resource "github_actions_environment_variable" "aws_github_actions_iam_role_arn" {
+  repository    = github_repository_environment.main.repository
+  environment   = github_repository_environment.main.environment
+  variable_name = "AWS_GITHUB_ACTIONS_IAM_ROLE_ARN"
+  value         = aws_iam_role.github_actions.arn
 }
