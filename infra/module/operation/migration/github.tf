c4ef0eb4ab8008d801bbfc42f4fd773a976a9faa
takaaki12353491 <takaaki12353491@gmail.com>
2024-09-08 22:47:45 +0900

    feat: AWS_MIGRATION_ECS_CLUSTER_NAMEを追加

--- infra/module/operation/migration/github.tf
+++ infra/module/operation/migration/github.tf
@@ -5,6 +5,13 @@ resource "github_actions_environment_variable" "aws_migration_ecr_url" {
   value         = aws_ecr_repository.migration.repository_url
 }
 
+resource "github_actions_environment_variable" "aws_migration_ecs_cluster_name" {
+  repository    = var.github_repository
+  environment   = var.env
+  variable_name = "AWS_MIGRATION_ECS_CLUSTER_NAME"
+  value         = aws_ecs_cluster.migration.name
+}
+
 resource "github_actions_environment_variable" "aws_migration_ecs_task_family" {
   repository    = var.github_repository
   environment   = var.env
