1116d01e8e6aec5ada4e822c784abb7125241ae6
takaaki12353491 <takaaki12353491@gmail.com>
2024-09-08 16:19:39 +0900

    feat: AWS_MIGRATION_ECS_TASK_FAMILYをgithubに追加

--- infra/module/operation/migration/github.tf
+++ infra/module/operation/migration/github.tf
@@ -4,3 +4,10 @@ resource "github_actions_environment_variable" "aws_migration_ecr_url" {
   variable_name = "AWS_MIGRATION_ECR_URL"
   value         = aws_ecr_repository.migration.repository_url
 }
+
+resource "github_actions_environment_variable" "aws_migration_ecs_task_family" {
+  repository    = var.github_repository
+  environment   = var.env
+  variable_name = "AWS_MIGRATION_ECS_TASK_FAMILY"
+  value         = aws_ecs_task_definition.migration.family
+}
