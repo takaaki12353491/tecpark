0f1fd710bf9365663140e8ba49dca9951e8daae6
takaaki12353491 <takaaki12353491@gmail.com>
2024-09-16 03:31:28 +0900

    feat: デフォルトブランチを定義

--- infra/env/share/github/github_branch.tf
+++ infra/env/share/github/github_branch.tf
@@ -2,3 +2,8 @@ resource "github_branch" "main" {
   repository = github_repository.main.name
   branch     = "main"
 }
+
+resource "github_branch_default" "main" {
+  repository = github_repository.main.name
+  branch     = github_branch.main.branch
+}
