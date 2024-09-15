25a302eb82e8a85c6a89d7e58701d2588ea93fcc
takaaki12353491 <takaaki12353491@gmail.com>
2024-09-16 07:35:21 +0900

    feat: squash merge限定にする

--- infra/env/share/github/github_repository.tf
+++ infra/env/share/github/github_repository.tf
@@ -7,4 +7,8 @@ resource "github_repository" "main" {
   is_template  = true
 
   delete_branch_on_merge = true
+
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = false
 }
