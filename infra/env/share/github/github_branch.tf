5fb18c4646955364d1885189e063883dbdd3d58f
takaaki12353491 <takaaki12353491@gmail.com>
2024-09-16 04:29:24 +0900

    feat: ブランチ保護ルールを作成

--- infra/env/share/github/github_branch.tf
+++ infra/env/share/github/github_branch.tf
@@ -7,3 +7,14 @@ resource "github_branch_default" "main" {
   repository = github_repository.main.name
   branch     = github_branch.main.branch
 }
+
+resource "github_branch_protection" "main" {
+  repository_id = github_repository.main.node_id
+  pattern       = github_branch.main.branch
+
+  allows_deletions                = false
+  allows_force_pushes             = false
+  enforce_admins                  = true
+  require_conversation_resolution = true
+  required_linear_history         = true
+}
