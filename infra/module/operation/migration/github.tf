b14ab22528704e02b49769d55a15f4138b5cdc52
takaaki12353491 <takaaki12353491@gmail.com>
2024-09-16 00:39:34 +0900

    feat: AWS_MIGRATION_ECR_REPOSITORY_NAMEを追加

--- infra/module/operation/migration/github.tf
+++ infra/module/operation/migration/github.tf
@@ -5,6 +5,13 @@ resource "github_actions_environment_variable" "aws_migration_ecr_url" {
   value         = aws_ecr_repository.migration.repository_url
 }
 
+resource "github_actions_environment_variable" "aws_migration_ecr_repository_name" {
+  repository    = var.github_repository
+  environment   = var.env
+  variable_name = "AWS_MIGRATION_ECR_REPOSITORY_NAME"
+  value         = aws_ecr_repository.migration.name
+}
+
 resource "github_actions_environment_variable" "aws_migration_ecs_cluster_name" {
   repository    = var.github_repository
   environment   = var.env
