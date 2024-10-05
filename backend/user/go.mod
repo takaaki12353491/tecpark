e144a143c8f7017e9734b49e3f8e2468feccaeda
takaaki12353491 <takaaki12353491@gmail.com>
2024-10-05 22:40:14 +0900

    feat: echoでサーバーを建てる

--- backend/user/go.mod
+++ backend/user/go.mod
@@ -4,6 +4,7 @@ go 1.22.5
 
 require (
 	github.com/99designs/gqlgen v0.17.55
+	github.com/labstack/echo/v4 v4.12.0
 	github.com/vektah/gqlparser/v2 v2.5.17
 )
 
@@ -12,7 +13,15 @@ require (
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
+	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/sosodev/duration v1.3.1 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	golang.org/x/crypto v0.28.0 // indirect
 	golang.org/x/net v0.30.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
 )
--- backend/user/main.go
+++ backend/user/main.go
@@ -2,28 +2,22 @@ package main
 
 import (
 	"log"
-	"net/http"
-	"os"
 	"user/internal/interface/graphql"
 	"user/internal/interface/graphql/resolver"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/labstack/echo/v4"
 )
 
-const defaultPort = "8080"
-
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = defaultPort
-	}
-
 	srv := handler.NewDefaultServer(graphql.NewExecutableSchema(graphql.Config{Resolvers: &resolver.Resolver{}}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	e := echo.New()
+	e.GET("/playground", echo.WrapHandler(playground.Handler("GraphQL playground", "/playground")))
+	e.GET("/query", echo.WrapHandler(srv))
 
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	port := "8080"
+	log.Printf("connect to http://localhost:%s/playground for GraphQL playground", port)
+	e.Logger.Fatal(e.Start(":" + port))
 }
