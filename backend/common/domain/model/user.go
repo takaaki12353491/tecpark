918ed1809458f2af32efa14dd23af72515e2d733
takaaki12353491 <takaaki12353491@gmail.com>
2024-10-05 23:28:10 +0900

    fix: IDをULIDに修正

--- backend/common/domain/model/user.go
+++ backend/common/domain/model/user.go
@@ -5,7 +5,7 @@ import (
 )
 
 type User struct {
-	ID        uint      `gorm:"primarykey"`
+	ID        ULID      `gorm:"primarykey;type:char(26)"`
 	CreatedAt time.Time `gorm:"not null;default:CURRENT_TIMESTAMP(3)"`
 	UpdatedAt time.Time `gorm:"not null;default:CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3)"`
 }
