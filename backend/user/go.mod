b467ed088ca8f97747bbd2b491d445ea2d2076fb
takaaki12353491 <takaaki12353491@gmail.com>
2024-10-05 19:30:30 +0900

    feat: graphqlのボイラープレートを作成

--- backend/user/go.mod
+++ backend/user/go.mod
@@ -1,3 +1,14 @@
 module user
 
 go 1.22.5
+
+require (
+	github.com/99designs/gqlgen v0.17.55
+	github.com/vektah/gqlparser/v2 v2.5.17
+)
+
+require (
+	github.com/agnivade/levenshtein v1.1.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/sosodev/duration v1.3.1 // indirect
+)
--- backend/user/main.go
+++ backend/user/main.go
@@ -1,48 +1,28 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
-	"strings"
+	"os"
+	"user/graph"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
 )
 
-type Response struct {
-	Message string `json:"message"`
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	log.Printf("アクセスがありました: %s %s", r.Method, r.URL.Path)
-	response := Response{Message: "Hello, World!"}
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(response)
-}
-
-func corsMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		origin := r.Header.Get("Origin")
-		if origin != "" && isAllowedOrigin(origin) {
-			w.Header().Set("Access-Control-Allow-Origin", origin)
-		}
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-		next.ServeHTTP(w, r)
-	})
-}
-
-// サブドメインとルートドメインをチェックする関数
-func isAllowedOrigin(origin string) bool {
-	allowedDomain := "stg.tecpark.net"
-	// サブドメインもしくはルートドメインが一致するかどうかをチェック
-	return origin == "http://"+allowedDomain || origin == "https://"+allowedDomain || strings.HasSuffix(origin, "."+allowedDomain)
-}
+const defaultPort = "8080"
 
 func main() {
-	log.Println("サーバーが起動しました。ポート: 80")
-	http.Handle("/", corsMiddleware(http.HandlerFunc(handler)))
-	http.ListenAndServe(":80", nil)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
