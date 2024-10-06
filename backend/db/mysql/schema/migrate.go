100b63046b0cf98ba296974857db9ee9f0590995
takaaki12353491 <takaaki12353491@gmail.com>
2024-10-06 19:38:33 +0900

    feat: migrataionにロギングを設定

--- backend/db/mysql/schema/migrate.go
+++ backend/db/mysql/schema/migrate.go
@@ -3,10 +3,13 @@ package main
 import (
 	"common/domain/model"
 	"fmt"
+	"log"
 	"os"
+	"time"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func main() {
@@ -17,7 +20,18 @@ func main() {
 	database := os.Getenv("MYSQL_DATABASE")
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", user, password, host, port, database)
 
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	logger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold: time.Second,
+			LogLevel:      logger.Info,
+			Colorful:      true,
+		},
+	)
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: logger,
+	})
 	if err != nil {
 		panic(fmt.Sprintf("failed to connect to database: %v", err))
 	}
