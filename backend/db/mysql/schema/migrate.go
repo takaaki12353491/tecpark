d826943f1cd05200deb86c48ffa5853e204b511c
takaaki12353491 <takaaki12353491@gmail.com>
2024-10-07 19:42:52 +0900

    ref: migrateで共通関数を使う

--- backend/db/mysql/schema/migrate.go
+++ backend/db/mysql/schema/migrate.go
@@ -1,16 +1,11 @@
 package main
 
 import (
+	"common/db"
 	"common/domain/model"
 	"common/util"
 	"fmt"
-	"net/url"
-	"os"
 	"time"
-
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 )
 
 func main() {
@@ -21,20 +16,7 @@ func main() {
 	}
 	time.Local = location
 
-	user := os.Getenv("MYSQL_USER")
-	password := os.Getenv("MYSQL_PASSWORD")
-	host := os.Getenv("MYSQL_HOST")
-	port := os.Getenv("MYSQL_PORT")
-	database := os.Getenv("MYSQL_DATABASE")
-	tzEncoded := url.QueryEscape(tz)
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=%s", user, password, host, port, database, tzEncoded)
-
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Info),
-	})
-	if err != nil {
-		panic(fmt.Sprintf("failed to connect to database: %v", err))
-	}
+	db, _ := db.NewDB()
 
 	db.AutoMigrate(&model.User{})
 }
