59f977fb2d784749a585d25021529e6f8ddc8bb8
takaaki12353491 <takaaki12353491@gmail.com>
2024-10-08 02:36:01 +0900

    feat: DBの設定を外部から受け取れるようにする

--- backend/common/db/connection.go
+++ backend/common/db/connection.go
@@ -11,15 +11,42 @@ import (
 	"gorm.io/gorm/logger"
 )
 
-func NewDB() (*gorm.DB, error) {
-	user := os.Getenv("MYSQL_USER")
-	password := os.Getenv("MYSQL_PASSWORD")
-	host := os.Getenv("MYSQL_HOST")
-	port := os.Getenv("MYSQL_PORT")
-	database := os.Getenv("MYSQL_DATABASE")
-	tz := util.GetEnv("TZ", "Asia/Tokyo")
-	tzEncoded := url.QueryEscape(tz)
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=%s", user, password, host, port, database, tzEncoded)
+type Config struct {
+	User     string
+	Password string
+	Host     string
+	Port     string
+	Database string
+	TZ       string
+}
+
+func (c *Config) getDSN() string {
+	tzEncoded := url.QueryEscape(c.TZ)
+	return fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=%s",
+		c.User, c.Password, c.Host, c.Port, c.Database, tzEncoded)
+}
+
+type Option func(*Config)
+
+func WithTZ(tz string) Option {
+	return func(c *Config) {
+		c.TZ = tz
+	}
+}
+
+func NewDB(options ...Option) (*gorm.DB, error) {
+	config := &Config{
+		User:     os.Getenv("MYSQL_USER"),
+		Password: os.Getenv("MYSQL_PASSWORD"),
+		Host:     os.Getenv("MYSQL_HOST"),
+		Port:     os.Getenv("MYSQL_PORT"),
+		Database: os.Getenv("MYSQL_DATABASE"),
+		TZ:       util.GetEnv("TZ", "Asia/Tokyo"),
+	}
+	for _, opt := range options {
+		opt(config)
+	}
+	dsn := config.getDSN()
 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
