2e4bf2e39fef91935fd306eaaa9fc0cf59bf2e8d
takaaki12353491 <takaaki12353491@gmail.com>
2024-10-13 11:12:22 +0900

    ref: ログのメッセージを修正

--- backend/common/log/logger.go
+++ backend/common/log/logger.go
@@ -58,24 +58,27 @@ func (l *Logger) Trace(ctx context.Context, begin time.Time, fc func() (sql stri
 
 	elapsed := time.Since(begin)
 	sql, rows := fc()
+	latency := float64(elapsed.Nanoseconds()) / 1e6
+
 	switch {
 	case err != nil && l.LogLevel >= logger.Error:
-		if rows == -1 {
-			slog.ErrorContext(ctx, fmt.Sprintf("[%.3fms] [rows:%s] %s", float64(elapsed.Nanoseconds())/1e6, "-", sql), slog.String("error", err.Error()))
-		} else {
-			slog.ErrorContext(ctx, fmt.Sprintf("[%.3fms] [rows:%d] %s", float64(elapsed.Nanoseconds())/1e6, rows, sql), slog.String("error", err.Error()))
-		}
+		slog.ErrorContext(ctx, "sql is error",
+			slog.String("latency", fmt.Sprintf("%.3fms", latency)),
+			slog.Int64("rows", rows),
+			slog.String("sql", sql),
+			slog.String("error", err.Error()),
+		)
 	case elapsed > l.SlowThreshold && l.SlowThreshold != 0 && l.LogLevel >= logger.Warn:
-		if rows == -1 {
-			slog.WarnContext(ctx, fmt.Sprintf("[%.3fms] [rows:%s] %s", float64(elapsed.Nanoseconds())/1e6, "-", sql))
-		} else {
-			slog.WarnContext(ctx, fmt.Sprintf("[%.3fms] [rows:%d] %s", float64(elapsed.Nanoseconds())/1e6, rows, sql))
-		}
+		slog.WarnContext(ctx, "sql is slow",
+			slog.String("latency", fmt.Sprintf("%.3fms", latency)),
+			slog.Int64("rows", rows),
+			slog.String("sql", sql),
+		)
 	case l.LogLevel == logger.Info:
-		if rows == -1 {
-			slog.InfoContext(ctx, fmt.Sprintf("[%.3fms] [rows:%s] %s", float64(elapsed.Nanoseconds())/1e6, "-", sql))
-		} else {
-			slog.InfoContext(ctx, fmt.Sprintf("[%.3fms] [rows:%d] %s", float64(elapsed.Nanoseconds())/1e6, rows, sql))
-		}
+		slog.InfoContext(ctx, "sql is success",
+			slog.String("latency", fmt.Sprintf("%.3fms", latency)),
+			slog.Int64("rows", rows),
+			slog.String("sql", sql),
+		)
 	}
 }
