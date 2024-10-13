aacbc2c600c95682ced591301d062b379ad3eccb
takaaki12353491 <takaaki12353491@gmail.com>
2024-10-13 09:03:47 +0900

    fix: ロガーの設定を変更

--- backend/user/main.go
+++ backend/user/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"common/db"
 	"common/db/query"
 	"common/util"
@@ -16,6 +17,7 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 	"go.opentelemetry.io/contrib/instrumentation/github.com/labstack/echo/otelecho"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	"go.opentelemetry.io/otel/trace"
 )
 
 func main() {
@@ -34,8 +36,23 @@ func main() {
 	e := echo.New()
 
 	e.Use(middleware.Recover())
-	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{}))
-	e.Use(otelecho.Middleware("tecpark-user", otelecho.WithTracerProvider(sdktrace.NewTracerProvider())))
+	tp := sdktrace.NewTracerProvider()
+	e.Use(otelecho.Middleware("tecpark-user", otelecho.WithTracerProvider(tp))) //先に設定しないとtraceIDなどがcontextに設定されない
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Skipper: func(c echo.Context) bool {
+			return c.Path() == "/metrics" || c.Path() == "/health"
+		},
+		Format: `{"time":"${time_rfc3339_nano}",${custom},"remote_ip":"${remote_ip}",` +
+			`"host":"${host}","method":"${method}","uri":"${uri}","status":${status},` +
+			`"error":"${error}","latency":${latency},"latency_human":"${latency_human}",` +
+			`"bytes_in":${bytes_in},"bytes_out":${bytes_out}}` + "\n",
+		CustomTagFunc: func(c echo.Context, buf *bytes.Buffer) (int, error) {
+			span := trace.SpanFromContext(c.Request().Context())
+			buf.WriteString(fmt.Sprintf("\"%s\":\"%s\"", "traceID", span.SpanContext().TraceID().String()))
+			buf.WriteString(fmt.Sprintf(",\"%s\":\"%s\"", "spanID", span.SpanContext().SpanID().String()))
+			return 0, nil
+		},
+	}))
 
 	e.POST("/query", echo.WrapHandler(srv))
 	e.GET("/playground", echo.WrapHandler(playground.Handler("GraphQL playground", "/query")))
