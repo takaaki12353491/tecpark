3918212de0d3668bfc709f8ad639ac6b7ec79e09
takaaki12353491 <takaaki12353491@gmail.com>
2024-10-14 15:15:21 +0900

    test: UserServiceのテストを作成

---
+++ backend/user/internal/service/user_service_test.go
@@ -0,0 +1,65 @@
+package service
+
+import (
+	"common/domain/model"
+	"context"
+	"errors"
+	"testing"
+	"user/internal/domain/repository"
+
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+)
+
+var _ repository.UserRepository = (*MockUserRepository)(nil)
+
+type MockUserRepository struct {
+	mock.Mock
+}
+
+func (m *MockUserRepository) GetUsers(ctx context.Context) ([]*model.User, error) {
+	args := m.Called(ctx)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).([]*model.User), args.Error(1)
+}
+
+func TestUserService(t *testing.T) {
+	suite.Run(t, &UserServiceSuite{})
+}
+
+type UserServiceSuite struct {
+	suite.Suite
+	userService    *UserService
+	userRepository *MockUserRepository
+}
+
+func (s *UserServiceSuite) SetupTest() {
+	s.userRepository = new(MockUserRepository)
+	s.userService = NewUserService(s.userRepository)
+}
+
+func (s *UserServiceSuite) TestGetUsers_Success() {
+	expectedUsers := []*model.User{
+		{ID: "1", Nickname: "1"},
+		{ID: "2", Nickname: "2"},
+	}
+	s.userRepository.On("GetUsers", mock.Anything).Return(expectedUsers, nil)
+
+	result, err := s.userService.GetUsers(context.Background())
+
+	s.NoError(err)
+	s.Equal(expectedUsers, result)
+}
+
+func (s *UserServiceSuite) TestGetUsers_Error() {
+	expectedError := errors.New("failed to get users")
+	s.userRepository.On("GetUsers", mock.Anything).Return(nil, expectedError)
+
+	result, err := s.userService.GetUsers(context.Background())
+
+	s.Error(err)
+	s.Nil(result)
+	s.Equal(expectedError, err)
+}
