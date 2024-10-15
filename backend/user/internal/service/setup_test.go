c0de2c63b04ac3a83294391ce334a6903574df8e
takaaki12353491 <takaaki12353491@gmail.com>
2024-10-15 20:31:17 +0900

    test: TestMainでトランザクションを貼らない

--- backend/user/internal/service/setup_test.go
+++ backend/user/internal/service/setup_test.go
@@ -3,31 +3,24 @@ package service
 import (
 	"common/db"
 	"common/domain/model"
-	"log"
 	"os"
 	"testing"
 
 	"gorm.io/gorm"
 )
 
-var testTX *gorm.DB
+var testDB *gorm.DB
 
 func TestMain(m *testing.M) {
-	testDB, _ := db.NewDB(db.WithPort("33306"))
+	testDB, _ = db.NewDB(db.WithPort("33306"))
 
-	testTX = testDB.Begin()
-	if err := testTX.Error; err != nil {
-		log.Fatalf("failed to begin transaction: %v", err)
-	}
-
-	if err := testTX.AutoMigrate(&model.User{}); err != nil {
-		log.Fatalf("failed to migrate database: %v", err)
-	}
+	testDB.AutoMigrate(&model.User{})
 
 	code := m.Run()
 
-	if err := testTX.Rollback().Error; err != nil {
-		log.Fatalf("failed to rollback transaction: %v", err)
+	tables, _ := testDB.Migrator().GetTables()
+	for _, table := range tables {
+		testDB.Migrator().DropTable(table)
 	}
 
 	os.Exit(code)
