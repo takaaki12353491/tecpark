947b11a7d9503aee03ddde1a477dbe27e4ce2d44
takaaki12353491 <takaaki12353491@gmail.com>
2024-10-20 14:14:00 +0900

    chore: viteの設定を元に戻す

--- frontend/user/vite.config.ts
+++ frontend/user/vite.config.ts
@@ -2,6 +2,12 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+declare module "@remix-run/node" {
+  interface Future {
+    v3_singleFetch: true;
+  }
+}
+
 export default defineConfig({
   plugins: [
     remix({
