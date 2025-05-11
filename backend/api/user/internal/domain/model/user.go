3022b394762d9ec3ca07b7ed642e48afd035a8a5
takaaki12353491 <takaaki12353491@gmail.com>
2025-05-11 22:14:30 +0900

    ref: modelを移行

---
+++ backend/api/user/internal/domain/model/user.go
@@ -0,0 +1,14 @@
+package model
+
+import (
+	"time"
+
+	"github.com/oklog/ulid/v2"
+)
+
+type User struct {
+	ID        ulid.ULID `gorm:"primarykey;type:char(26)"`
+	Nickname  string    `gorm:"not null;type:varchar(30);"`
+	CreatedAt time.Time `gorm:"not null;default:CURRENT_TIMESTAMP(3)"`
+	UpdatedAt time.Time `gorm:"not null;default:CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3)"`
+}
