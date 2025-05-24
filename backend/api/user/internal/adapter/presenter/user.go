9be2801e1f39f867767206d1bb0d1f05231a5a26
takaaki12353491 <takaaki12353491@gmail.com>
2025-05-24 15:30:46 +0900

    feat: presenterを作成

---
+++ backend/api/user/internal/adapter/presenter/user.go
@@ -0,0 +1,27 @@
+package presenter
+
+import (
+	"user/internal/domain/model"
+	odata "user/internal/usecase/output/data"
+)
+
+type User struct {
+}
+
+func NewUser() *User {
+	return &User{}
+}
+
+func (u *User) Present(users []*model.User) ([]*odata.User, error) {
+	odataUsers := make([]*odata.User, 0, len(users))
+	for _, user := range users {
+		odataUsers = append(odataUsers, &odata.User{
+			ID:        user.ID.String(),
+			Nickname:  user.Nickname,
+			CreatedAt: user.CreatedAt,
+			UpdatedAt: user.UpdatedAt,
+		})
+	}
+
+	return odataUsers, nil
+}
